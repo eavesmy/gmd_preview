aae69ac5dbfecf1746f57618304b3ab2da4c5e35
eavesmy <eavesmy@gmail.com>
2020-03-31 19:20:53 +0800

    base

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "fmt"
-import "flag"
-import "net/http"
+import (
+"github.com/fsnotify/fsnotify"
+"io/ioutil"
+"bytes"
+"fmt"
+"io"
+"flag"
+"text/template"
+"github.com/gorilla/websocket"
+"net/http"
+)
 
 var FILE string
 var CSS string
+var PORT string
+var reloadChan = make(chan bool)
+
+type Md struct {
+    Title string
+    Css string
+    Markdown string
+}
+
+var md *Md
 
 func init() {
+	flag.StringVar(&PORT, "port", "8080", "Service port")
 	flag.StringVar(&FILE, "file", "", "Markdown file path")
 	flag.StringVar(&CSS, "css", "https://eva7base.com/css/sspai.css", "Markdown css file.Can use network location or local path.")
 	flag.Parse()
@@ -15,19 +34,83 @@ func init() {
 	if FILE == "" {
 		panic("Require markdown file path.")
 	}
-	fmt.Println(FILE)
+
+    md = &Md{}
+
+    md.Title = FILE
+    md.Css = CSS
+    md.Markdown = ""
 }
 
 func main() {
-
+    
 	// temp := ""
 
-	serve()
+    go watcher()
+
+	var handler http.HandlerFunc = func(res http.ResponseWriter, req *http.Request) {
+
+        ws := websocket.Upgrader{}
+        conn,err := ws.Upgrade(res,req,nil)
+
+        if err != nil {
+            fmt.Println(err)
+        }
+
+        
+        for{
+            <-reloadChan
+            data := []byte{}
+            buffer := bytes.NewBuffer(data)
+            Html(buffer)
+            conn.WriteMessage(0,data)
+        }
+	}
+    fmt.Println("Service start at 8080")
+	if err := http.ListenAndServe(":" + PORT, handler); err != nil {
+        panic(err)
+    }
 }
 
-func serve() {
-	var handler http.HandlerFunc = func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte("Hello"))
-	}
-	http.ListenAndServe(":8080", handler)
+func Html(w io.Writer){
+    temp := template.New("html")
+    temp.Parse(`
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{.Css}}">
+    <title>{{.Title}}</title>
+    <body>
+        <div id="content"></div>
+        <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+        <script>
+            document.getElementById('content').innerHTML =
+            marked("{{.Markdown}}");
+        </script>
+    </body>
+</html>
+    `)
+    temp.Execute(w,md)
+}
+
+func watcher(){
+    watch,err:=fsnotify.NewWatcher()
+    if err != nil {
+        panic(err)
+    }
+    defer watch.Close()
+
+    err = watch.Add(FILE)
+
+    for {
+        event := <- watch.Events
+
+        if event.Op&fsnotify.Write == fsnotify.Write {
+            buffer, err := ioutil.ReadFile(FILE)
+            if err != nil {
+                panic(err)
+            }
+            md.Markdown = string(buffer)
+            reloadChan <- true
+        }
+    }
 }
